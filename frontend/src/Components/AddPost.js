c636f1c84e489277f3458d93fd9fda52ee114087
peddiashrith <ashrithkumarpeddi@gmail.com>
2020-08-07 21:36:50 +0530

    create post over

--- frontend/src/Components/AddPost.js
+++ frontend/src/Components/AddPost.js
@@ -6,9 +6,12 @@ import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import PostAddIcon from "@material-ui/icons/PostAdd";
 import Typography from "@material-ui/core/Typography";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import MultiTags from "./MultiTags";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import RemoveCircleIcon from "@material-ui/icons/RemoveCircle";
+import IconButton from "@material-ui/core/IconButton";
 
 const useStyles = (theme) => ({
     paper: {
@@ -16,6 +19,10 @@ const useStyles = (theme) => ({
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
+    },
+    linkHeading: {
+        ...theme.typography.button,
+        color: "blue",
     },
     avatar: {
         margin: theme.spacing(1),
@@ -28,6 +35,12 @@ const useStyles = (theme) => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
+    tag: {
+        width: 400,
+        "& > * + *": {
+            marginTop: theme.spacing(3),
+        },
+    },
 });
 export class AddPost extends Component {
     constructor(props) {
@@ -36,23 +49,56 @@ export class AddPost extends Component {
         this.state = {
             post_title: "",
             post_description: "",
+            all_post_tags: [],
             post_tags: [],
-            post_links: [],
+            post_links: [{ name: "google.com" }, { name: "iitdh.ac.in" }],
         };
     }
 
+    componentDidMount = async () => {
+        this.setState({
+            all_post_tags: top100Films,
+        });
+    };
+
+    handleLinkChange(event, index) {
+        const values = [...this.state.post_links];
+        values[index] = { name: event.target.value };
+
+        this.setState({
+            post_links: values,
+        });
+    }
+
+    handleAddLink() {
+        const values = [...this.state.post_links];
+        values.push({ name: "" });
+        this.setState({
+            post_links: values,
+        });
+    }
+
+    handleRemoveLink(event, index) {
+        const values = [...this.state.post_links];
+        values.splice(index, 1);
+        this.setState({
+            post_links: values,
+        });
+    }
+
     handleChange = (event) => {
-        this.setState(
-            {
-                [event.target.name]: event.target.value,
-            },
-            console.log(`After changinng ${this.state}`)
-        );
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    handleTags = (event, newValue) => {
+        this.setState({
+            post_tags: newValue,
+        });
     };
 
     handleSubmit = (event) => {
         event.preventDefault();
-        console.log("Clicked");
+        console.log(this.state);
     };
 
     render() {
@@ -73,6 +119,7 @@ export class AddPost extends Component {
                                 <TextField
                                     name="post_title"
                                     value={this.state.post_title}
+                                    onChange={this.handleChange}
                                     variant="outlined"
                                     required
                                     fullWidth
@@ -84,6 +131,8 @@ export class AddPost extends Component {
                             <Grid item xs={12}>
                                 <TextField
                                     name="post_description"
+                                    value={this.state.post_description}
+                                    onChange={this.handleChange}
                                     variant="outlined"
                                     required
                                     multiline
@@ -95,7 +144,82 @@ export class AddPost extends Component {
                                 />
                             </Grid>
                             <Grid item xs={12}>
-                                <MultiTags />
+                                <div className={classes.tag}>
+                                    <Autocomplete
+                                        multiple
+                                        size="small"
+                                        limitTags={3}
+                                        id="post_tags"
+                                        options={this.state.all_post_tags}
+                                        onChange={this.handleTags}
+                                        getOptionLabel={(option) =>
+                                            option.title
+                                        }
+                                        defaultValue={[]}
+                                        renderInput={(params) => (
+                                            <TextField
+                                                {...params}
+                                                variant="outlined"
+                                                label="Post Tags"
+                                                placeholder="Favorites"
+                                            />
+                                        )}
+                                    />
+                                </div>
+                            </Grid>
+                            <Grid item sm={12}>
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "space-between",
+                                        alignItems: "center",
+                                    }}
+                                    className={classes.linkHeading}
+                                >
+                                    <Typography variant="h6" color="orange">
+                                        Add Links
+                                    </Typography>
+                                    <IconButton
+                                        onClick={(event) =>
+                                            this.handleAddLink()
+                                        }
+                                    >
+                                        <AddCircleOutlineIcon />
+                                    </IconButton>
+                                </div>
+                            </Grid>
+                            <Grid item xs={12}>
+                                {this.state.post_links.map((tag, index) => (
+                                    <div
+                                        key={index}
+                                        style={{
+                                            display: "flex",
+                                            justifyContent: "space-around",
+                                        }}
+                                    >
+                                        <TextField
+                                            fullWidth
+                                            name="post_tag"
+                                            value={tag.name}
+                                            onChange={(event) =>
+                                                this.handleLinkChange(
+                                                    event,
+                                                    index
+                                                )
+                                            }
+                                        />
+                                        <IconButton
+                                            onClick={(event) =>
+                                                this.handleRemoveLink(
+                                                    event,
+                                                    index
+                                                )
+                                            }
+                                        >
+                                            <RemoveCircleIcon />
+                                        </IconButton>
+                                    </div>
+                                ))}
                             </Grid>
                         </Grid>
                         <Button
@@ -114,5 +238,113 @@ export class AddPost extends Component {
         );
     }
 }
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films = [
+    { title: "The Shawshank Redemption", year: 1994 },
+    { title: "The Godfather", year: 1972 },
+    { title: "The Godfather: Part II", year: 1974 },
+    { title: "The Dark Knight", year: 2008 },
+    { title: "12 Angry Men", year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: "Pulp Fiction", year: 1994 },
+    { title: "The Lord of the Rings: The Return of the King", year: 2003 },
+    { title: "The Good, the Bad and the Ugly", year: 1966 },
+    { title: "Fight Club", year: 1999 },
+    { title: "The Lord of the Rings: The Fellowship of the Ring", year: 2001 },
+    { title: "Star Wars: Episode V - The Empire Strikes Back", year: 1980 },
+    { title: "Forrest Gump", year: 1994 },
+    { title: "Inception", year: 2010 },
+    { title: "The Lord of the Rings: The Two Towers", year: 2002 },
+    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+    { title: "Goodfellas", year: 1990 },
+    { title: "The Matrix", year: 1999 },
+    { title: "Seven Samurai", year: 1954 },
+    { title: "Star Wars: Episode IV - A New Hope", year: 1977 },
+    { title: "City of God", year: 2002 },
+    { title: "Se7en", year: 1995 },
+    { title: "The Silence of the Lambs", year: 1991 },
+    { title: "It's a Wonderful Life", year: 1946 },
+    { title: "Life Is Beautiful", year: 1997 },
+    { title: "The Usual Suspects", year: 1995 },
+    { title: "Léon: The Professional", year: 1994 },
+    { title: "Spirited Away", year: 2001 },
+    { title: "Saving Private Ryan", year: 1998 },
+    { title: "Once Upon a Time in the West", year: 1968 },
+    { title: "American History X", year: 1998 },
+    { title: "Interstellar", year: 2014 },
+    { title: "Casablanca", year: 1942 },
+    { title: "City Lights", year: 1931 },
+    { title: "Psycho", year: 1960 },
+    { title: "The Green Mile", year: 1999 },
+    { title: "The Intouchables", year: 2011 },
+    { title: "Modern Times", year: 1936 },
+    { title: "Raiders of the Lost Ark", year: 1981 },
+    { title: "Rear Window", year: 1954 },
+    { title: "The Pianist", year: 2002 },
+    { title: "The Departed", year: 2006 },
+    { title: "Terminator 2: Judgment Day", year: 1991 },
+    { title: "Back to the Future", year: 1985 },
+    { title: "Whiplash", year: 2014 },
+    { title: "Gladiator", year: 2000 },
+    { title: "Memento", year: 2000 },
+    { title: "The Prestige", year: 2006 },
+    { title: "The Lion King", year: 1994 },
+    { title: "Apocalypse Now", year: 1979 },
+    { title: "Alien", year: 1979 },
+    { title: "Sunset Boulevard", year: 1950 },
+    {
+        title:
+            "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
+        year: 1964,
+    },
+    { title: "The Great Dictator", year: 1940 },
+    { title: "Cinema Paradiso", year: 1988 },
+    { title: "The Lives of Others", year: 2006 },
+    { title: "Grave of the Fireflies", year: 1988 },
+    { title: "Paths of Glory", year: 1957 },
+    { title: "Django Unchained", year: 2012 },
+    { title: "The Shining", year: 1980 },
+    { title: "WALL·E", year: 2008 },
+    { title: "American Beauty", year: 1999 },
+    { title: "The Dark Knight Rises", year: 2012 },
+    { title: "Princess Mononoke", year: 1997 },
+    { title: "Aliens", year: 1986 },
+    { title: "Oldboy", year: 2003 },
+    { title: "Once Upon a Time in America", year: 1984 },
+    { title: "Witness for the Prosecution", year: 1957 },
+    { title: "Das Boot", year: 1981 },
+    { title: "Citizen Kane", year: 1941 },
+    { title: "North by Northwest", year: 1959 },
+    { title: "Vertigo", year: 1958 },
+    { title: "Star Wars: Episode VI - Return of the Jedi", year: 1983 },
+    { title: "Reservoir Dogs", year: 1992 },
+    { title: "Braveheart", year: 1995 },
+    { title: "M", year: 1931 },
+    { title: "Requiem for a Dream", year: 2000 },
+    { title: "Amélie", year: 2001 },
+    { title: "A Clockwork Orange", year: 1971 },
+    { title: "Like Stars on Earth", year: 2007 },
+    { title: "Taxi Driver", year: 1976 },
+    { title: "Lawrence of Arabia", year: 1962 },
+    { title: "Double Indemnity", year: 1944 },
+    { title: "Eternal Sunshine of the Spotless Mind", year: 2004 },
+    { title: "Amadeus", year: 1984 },
+    { title: "To Kill a Mockingbird", year: 1962 },
+    { title: "Toy Story 3", year: 2010 },
+    { title: "Logan", year: 2017 },
+    { title: "Full Metal Jacket", year: 1987 },
+    { title: "Dangal", year: 2016 },
+    { title: "The Sting", year: 1973 },
+    { title: "2001: A Space Odyssey", year: 1968 },
+    { title: "Singin' in the Rain", year: 1952 },
+    { title: "Toy Story", year: 1995 },
+    { title: "Bicycle Thieves", year: 1948 },
+    { title: "The Kid", year: 1921 },
+    { title: "Inglourious Basterds", year: 2009 },
+    { title: "Snatch", year: 2000 },
+    { title: "3 Idiots", year: 2009 },
+    { title: "Monty Python and the Holy Grail", year: 1975 },
+];
 
 export default withStyles(useStyles, { withTheme: true })(AddPost);
